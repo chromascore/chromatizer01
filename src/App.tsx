1350f55f466b3c1b83d8448588abedeccb417621
Takanori Hoshi <takanori.hoshi@gmail.com>
2019-03-31 00:02:35 -0700

    App.tsx のインデントを 4 に修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,59 +4,59 @@ import React from 'react';
 import * as v from './voice_analyse';
 
 const Buttons = (): JSX.Element => {
-  return <>
-    <div className="select">
-      <label htmlFor="audioSource">Audio source: </label><select id="audioSource"></select>
-    </div>
-    <button onClick={v.startRecording}>start analysis</button>
-    <button onClick={v.endRecording}>stop analysis</button>
-    <button onClick={v.toSharp}>♯Key(DoReMi)</button>
-    <button onClick={v.toFlat}>♭Key(DoReMi)</button>
-    <button onClick={v.toGermanSharp}>♯Key(German)</button>
-    <button onClick={v.toGermanFlat}>♭Key(German)</button>
-    <button onClick={v.toMono}>blackWhite</button>
-    <button onClick={v.toFilter0}>no filter</button>
-    <button onClick={v.toFilter1}>melody filter</button>
-    <button onClick={v.toFilter5}>chord filter</button>
-    <button onClick={v.adjust}>shift one note</button>
-  </>;
+    return <>
+        <div className="select">
+            <label htmlFor="audioSource">Audio source: </label><select id="audioSource"></select>
+        </div>
+        <button onClick={v.startRecording}>start analysis</button>
+        <button onClick={v.endRecording}>stop analysis</button>
+        <button onClick={v.toSharp}>♯Key(DoReMi)</button>
+        <button onClick={v.toFlat}>♭Key(DoReMi)</button>
+        <button onClick={v.toGermanSharp}>♯Key(German)</button>
+        <button onClick={v.toGermanFlat}>♭Key(German)</button>
+        <button onClick={v.toMono}>blackWhite</button>
+        <button onClick={v.toFilter0}>no filter</button>
+        <button onClick={v.toFilter1}>melody filter</button>
+        <button onClick={v.toFilter5}>chord filter</button>
+        <button onClick={v.adjust}>shift one note</button>
+    </>;
 };
 
 const Pitch = (): JSX.Element => {
-  const pitch = [
-    { name: 'C♭', relative: -1 },
-    { name: 'G♭', relative: -6 },
-    { name: 'D♭', relative: 1 },
-    { name: 'A♭', relative: -4 },
-    { name: 'E♭', relative: 3 },
-    { name: 'B♭', relative: -2 },
-    { name: 'F', relative: 5 },
-    { name: 'C', relative: 0 },
-    { name: 'G', relative: -5 },
-    { name: 'D', relative: 2 },
-    { name: 'A', relative: -3 },
-    { name: 'E', relative: 4 },
-    { name: 'B', relative: -1 },
-    { name: 'F♯', relative: 6 },
-    { name: 'C♯', relative: 1 },
-  ];
+    const pitch = [
+        { name: 'C♭', relative: -1 },
+        { name: 'G♭', relative: -6 },
+        { name: 'D♭', relative: 1 },
+        { name: 'A♭', relative: -4 },
+        { name: 'E♭', relative: 3 },
+        { name: 'B♭', relative: -2 },
+        { name: 'F', relative: 5 },
+        { name: 'C', relative: 0 },
+        { name: 'G', relative: -5 },
+        { name: 'D', relative: 2 },
+        { name: 'A', relative: -3 },
+        { name: 'E', relative: 4 },
+        { name: 'B', relative: -1 },
+        { name: 'F♯', relative: 6 },
+        { name: 'C♯', relative: 1 },
+    ];
 
-  return <>
-    <button onClick={v.toDefinite}>Absolute Pitch</button>
-    <p>Relative Pitch in:</p>
-    {pitch.map(each =>
-      <button key={each.name} onClick={() => v.toRelative(each.relative)}>{each.name}</button>
-    )}
-  </>;
+    return <>
+        <button onClick={v.toDefinite}>Absolute Pitch</button>
+        <p>Relative Pitch in:</p>
+        {pitch.map(each =>
+            <button key={each.name} onClick={() => v.toRelative(each.relative)}>{each.name}</button>
+        )}
+    </>;
 };
 
 const App = (): JSX.Element => {
-  return <>
-    <Buttons />
-    <hr />
-    <Pitch />
-    <hr />
-  </>;
+    return <>
+        <Buttons />
+        <hr />
+        <Pitch />
+        <hr />
+    </>;
 };
 
 export default App;
